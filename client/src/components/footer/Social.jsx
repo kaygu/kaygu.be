6a7489e97827e638a3bd30bedaf92e17ce670e38
kaygu <kaygumc@gmail.com>
2019-12-06 23:40:59 +0100

    add: icons

--- client/src/components/footer/Social.jsx
+++ client/src/components/footer/Social.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {makeStyles} from '@material-ui/core/styles'
+import Icon from '@material-ui/core/Icon';
 import {Link, List, ListItem, ListItemText, Tooltip} from '@material-ui/core'
 
 const useStyles = makeStyles(theme => ({
@@ -14,16 +15,16 @@ function Social() {
     const classes = useStyles()
 
     const social = [
-        {id: 1, name: "LinkedIn", link: "https://www.linkedin.com/in/camille-de-neef-60890a2b"},
-        {id: 2, name: "GitHub", link: "https://github.com/kaygu"},
-        {id: 3, name: "Twitter", link: "https://twitter.com/kaaygu"},
-        {id: 4, name: "Twitch", link: "https://www.twitch.tv/kaygu"}
+        {id: 1, name: "LinkedIn", link: "https://www.linkedin.com/in/camille-de-neef-60890a2b", icon: "LinkedIn"},
+        {id: 2, name: "GitHub", link: "https://github.com/kaygu", icon: "GitHub"},
+        {id: 3, name: "Twitter", link: "https://twitter.com/kaaygu", icon: "Twitter"},
+        {id: 4, name: "Twitch", link: "https://www.twitch.tv/kaygu", icon: ""}
         //add logo
     ];
     const content = social.map((social) => 
         <ListItem button key={social.id} component="a" href={social.link}>
             <Tooltip title={social.name}>
-                <ListItemText primary='test' />
+                <Icon>{social.icon}</Icon>
             </Tooltip>
         </ListItem>
     );
